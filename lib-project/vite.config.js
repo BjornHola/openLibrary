ec43588d49fad191eb20681cf84e197d5d4c906f
BjornHola <broadherstby@gmail.com>
2026-02-17 18:05:34 +0300

    configs: predeploy + fix configs

--- lib-project/vite.config.js
+++ lib-project/vite.config.js
@@ -1,28 +1,24 @@
-import { defineConfig } from "vite";
-import viteImagemin from "@vheemstra/vite-plugin-imagemin";
+import { defineConfig } from 'vite';
+import viteImagemin from 'vite-plugin-imagemin';
 
 export default defineConfig({
   plugins: [
     viteImagemin({
- optipng: {
-  optimizationLevel: 2,
-},
       pngquant: {
         quality: [0.7, 0.85],
         speed: 6,
       },
-
       svgo: {
         plugins: [
           {
-            name: "preset-default",
+            name: 'preset-default',
             params: {
               overrides: {
                 removeViewBox: false,
               },
             },
           },
-          "removeDimensions",
+          'removeDimensions',
         ],
       },
     }),
